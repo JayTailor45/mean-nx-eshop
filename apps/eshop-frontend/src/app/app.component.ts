76346ad96111b8988535067d8c935ed032723477
Jay Tailor <tailorj64@gmail.com>
2024-06-10 18:55:23 +0530

    draft shared lib components

--- apps/eshop-frontend/src/app/app.component.ts
+++ apps/eshop-frontend/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { ProductListComponent } from './pages/product-list/product-list.componen
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { BannerComponent } from '@eshop/ui';
 
 
 @Component({
   standalone: true,
-  imports: [RouterModule, ProductListComponent, HomePageComponent, HeaderComponent, FooterComponent],
+  imports: [RouterModule, ProductListComponent, HomePageComponent, HeaderComponent, FooterComponent, BannerComponent],
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-root',
   templateUrl: './app.component.html',
---
+++ libs/ui/src/lib/banner/banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './banner.component.html',
+})
+export class BannerComponent {
+
+}
