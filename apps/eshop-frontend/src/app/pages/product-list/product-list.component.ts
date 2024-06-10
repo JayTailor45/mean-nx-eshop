8f63f7312b98ef64633863f01bcf34e80229eccb
Jay Tailor <tailorj64@gmail.com>
2024-06-10 19:54:57 +0530

    styles initial setup

--- apps/eshop-frontend/src/app/pages/product-list/product-list.component.ts
+++ apps/eshop-frontend/src/app/pages/product-list/product-list.component.ts
@@ -5,7 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
 
