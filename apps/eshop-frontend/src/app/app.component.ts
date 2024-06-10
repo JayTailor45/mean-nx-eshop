e58db802c8d91a702446f34411afbca81e21b39d
Jay Tailor <tailorj64@gmail.com>
2024-06-10 18:11:04 +0530

    Initial setup

--- apps/eshop-frontend/src/app/app.component.ts
+++ apps/eshop-frontend/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, ProductListComponent, HomePageComponent, HeaderComponent, FooterComponent],
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- apps/eshop-frontend/src/app/app.routes.ts
+++ apps/eshop-frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => HomePageComponent
+  },
+  {
+    path: 'products',
+    loadComponent: () => ProductListComponent
+  }
+];
